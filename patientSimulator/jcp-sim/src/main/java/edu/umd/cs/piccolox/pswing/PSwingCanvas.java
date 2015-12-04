44df57a98d457ef3ed3a77209ac100dca5652955
esteban-aliverti <esteban.aliverti@gmail.com>
2015-12-04 14:38:48 -0300

    License headers added

--- patientSimulator/jcp-sim/src/main/java/edu/umd/cs/piccolox/pswing/PSwingCanvas.java
+++ patientSimulator/jcp-sim/src/main/java/edu/umd/cs/piccolox/pswing/PSwingCanvas.java
@@ -1,12 +1,17 @@
-/* Copyright 2003-2005, University of Colorado */
-
-/*
- * CVS Info -
- * Filename : $Source$
- * Branch : $Name$
- * Modified by : $Author: samreid $
- * Revision : $Revision: 13998 $
- * Date modified : $Date: 2007-03-22 17:51:34 -0600 (Thu, 22 Mar 2007) $
+/* 
+ * Copyright 2015 Cognitive Medical Systems, Inc (http://www.cognitivemedicine.com).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package edu.umd.cs.piccolox.pswing;
 
--- patientSimulator/jcp-sim/src/main/java/edu/umd/cs/piccolox/pswing/PSwingRepaintManager.java
+++ patientSimulator/jcp-sim/src/main/java/edu/umd/cs/piccolox/pswing/PSwingRepaintManager.java
@@ -1,12 +1,17 @@
-/* Copyright 2003-2005, University of Colorado */
-
-/*
- * CVS Info -
- * Filename : $Source: /fs/cvs/piccolo/piccolo/extras/edu/umd/cs/piccolox/pswing/PSwingRepaintManager.java,v $
- * Branch : $Name:  $
- * Modified by : $Author: jesse $
- * Revision : $Revision: 1.1 $
- * Date modified : $Date: 2006/01/05 16:54:26 $
+/* 
+ * Copyright 2015 Cognitive Medical Systems, Inc (http://www.cognitivemedicine.com).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package edu.umd.cs.piccolox.pswing;
 
